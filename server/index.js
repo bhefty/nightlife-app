bc93a93dd878eb8e72b542357669b63b319a16c2
Bill Hefty <billhefty@gmail.com>
2017-04-21 23:20:37 -0500

    refactor api routes out into its own file

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const path = require('path');
 
-const getAccessToken = require('./utilities/access_token')
-const getBarsByLocation = require('./utilities/search.js')
+const yelp = require('./routes/yelp')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,20 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-
-getAccessToken(process.env.CLIENT_ID,process.env.CLIENT_SECRET)
-  .then((result) => {
-    const ACCESS_TOKEN = result.access_token
-    getBarsByLocation(ACCESS_TOKEN, '79416')
-      .then((results) => console.log(results))
-  })
-  .catch(error => console.error(error))
-
-// Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-});
+app.use('/yelp', yelp)
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {

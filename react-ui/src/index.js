afc5f12958a304884224695013dfaf1ee4fcbafb
Bill Hefty <billhefty@gmail.com>
2017-04-30 01:19:24 -0500

    add redux tools enhancer

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger'
 import reducer from './reducers';
@@ -13,10 +13,11 @@ const middleware = [thunk]
 if(process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger())
 }
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
-  applyMiddleware(...middleware)
+  composeEnhancers(applyMiddleware(...middleware))
 )
 
 ReactDOM.render(

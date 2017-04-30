5d1d91e3249fc023b5abeaaac88a81cbb487b5e1
Bill Hefty <billhefty@gmail.com>
2017-04-29 22:17:02 -0500

    add bars view

---
+++ react-ui/src/modules/Bars/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Bars = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Bars!</h1>
+            <p>Welcome to Bars!</p>
+        </div>
+    )
+}
+
+export default Bars;

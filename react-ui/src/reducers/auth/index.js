36a13900c6d61efeac5d3ec040479f30c87866ee
Bill Hefty <billhefty@gmail.com>
2017-05-08 23:18:02 -0500

    add redcuer for auth

---
+++ react-ui/src/reducers/auth/index.js
@@ -0,0 +1,40 @@
+import * as types from '../../actions/types'
+
+const INITIAL_STATE = { error: '', message: '', content: '', authenticated: false }
+
+export default function (state = INITIAL_STATE, action) {
+    switch(action.type) {
+        case types.AUTH_USER:
+            return {
+                ...state,
+                error: '',
+                message: '',
+                fullName: action.payload,
+                authenticated: true
+            }
+        case types.UNAUTH_USER:
+            return {
+                ...state,
+                authenticated: false
+            }
+        case types.AUTH_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        case types.PROTECTED_TEST:
+            return {
+                ...state,
+                content: action.payload
+            }
+        case types.FETCH_USER_PROFILE:
+            return {
+                ...state,
+                profile: action.payload
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+}
